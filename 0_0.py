4b95d72ff278d9055a660500dc414c33c3d07428
dinamohamed85 <dina.elghareeb@gmail.com>
2024-04-23 23:39:39 +0200

    temperature example added

---
+++ 0_0.py
@@ -0,0 +1,69 @@
+from math import *
+# variable_name = Value (saved in Ram memory)
+id = 5
+print(5)
+print("hello")
+''' 
+this an integer variable
+to use in a calculator
+'''
+x = 10  # interger int variable
+print(x)
+
+y = 2.5  # float variable
+print(y)
+
+i = -10  # interger int variable
+print(i)
+
+j = -2.5  # float variable
+print(j)
+
+text = "dina"  # string variable
+print(text)
+
+ch = 'd'  # cahr variable
+print(ch)
+
+value = True  # boolean bool variable true or false
+print(value)
+
+# sum
+input_1 = 5  # int
+input_2 = 6  # int
+result = input_1 + input_2  # int
+print(result)
+
+input_3 = 1.1  # float
+input_4 = 3  # int
+result2 = input_4 - input_3  # float
+
+input_5 = "summtion= "
+# to concatenation use casting or conversion variable type to another type
+convert_result_to_string = str(result2)
+print(input_5 + convert_result_to_string)
+
+#user_nmae = input("plz enter your name :")
+#type , int()  , float()
+# + - * / % abs pow 3**2 min max ciel floor round sqrt
+phrase = ' enter your \' '
+phrase2 = ''' hallo ,
+            how are you'''
+# len() 
+print(phrase[0])    # -1 -2 -3 -4 -5 hello
+print(phrase.index('e'))
+#slicing
+print(phrase[1:4])
+print(phrase[-1:-4])
+#print(phrase[:-1])
+print(phrase[-1:])
+print(phrase[0:])
+print(phrase[-3:-8:-1])
+# upper() lower() find count('A') replace('Ali','saad')
+#concatenation
+# 3 methods
+# first_name + second_name
+# formated string format(first_name,second_name)
+# {first_name} , {second_name}
+#print(dir(phrase))
+#----------------------------------------------------------
---
+++ exercises/1-temperature.py
@@ -0,0 +1,23 @@
+'''
+    1- take input temperature(in celsius) from user
+    2- create conversion equation/formula to calculte temperature in fahrenheit
+    3- F = 9/5 * C +32
+    4- print the result {temperature from fahrenheit to celsius}
+    5- take input temperature(in fahrenheit) from user
+    6- create conversion equation/formula to calculte temperature in celsius
+    7- C = 5/9 * F -32
+    8- print the result {temperature from celsius to fahrenheit}
+'''
+
+# convert celsius to fahrenheit
+input_temprature_celsius = input("please enter the temperature in celsius  :")
+temprature_fahrenheit = (9/5 * float(input_temprature_celsius)) + 32
+print('the temprature  ', input_temprature_celsius,
+      ' C' + ' is ', temprature_fahrenheit, ' F')
+
+# convert fahrenheit to celsius
+input_temprature_fahrenheit = input(
+    "plz enter the temperature in fahrenheit  :")
+temprature_celsius = 5/9 * (float(input_temprature_fahrenheit) - 32)
+print('the temprature  ', input_temprature_fahrenheit,
+      ' F' ' is ', temprature_celsius, ' C')
